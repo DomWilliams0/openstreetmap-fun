ebc853df8e5ac923920300662159db5040c69204
Dom Williams <me@domwillia.ms>
2017-11-11 13:36:38 +0000

    Fix memory leak in world

--- src/world.c
+++ src/world.c
@@ -1,5 +1,6 @@
 #include "world.h"
 #include "error.h"
+#include "osm/osm.h"
 
 int init_world(struct world *world) {
 	vec_init(&world->roads);
@@ -7,6 +8,13 @@ int init_world(struct world *world) {
 }
 
 void free_world(struct world *world) {
+	int i = 0;
+	struct road r = {0};
+	vec_foreach(&world->roads, r, i) {
+		if (r.name != NULL)
+			free(r.name);
+	}
+
 	if (world->roads.data != NULL)
 		vec_deinit(&world->roads);
 }
