87c21c6124e85b74a112ff2e8d775cfe09e28a9a
Dom Williams <me@domwillia.ms>
2017-11-12 23:05:36 +0000

    Add command line arg support

--- src/main.c
+++ src/main.c
@@ -4,10 +4,12 @@
 #include "osm/parser.h"
 #include "world.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+
+	char *file = argc >= 2 ? argv[1] : "../xmls/place.xml";
 
 	struct world world;
-	int ret = parse_osm("../xmls/place.xml", &world);
+	int ret = parse_osm(file, &world);
 
 	if (ret != CRACKING) {
 		printf("error: %s\n", error_get_message(ret));
